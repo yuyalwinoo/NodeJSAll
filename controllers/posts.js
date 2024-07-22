ba06f4e2b4338d4987458cba8b6b853d47564074
yuyalwinoo <yuyal1593@gmail.com>
2024-07-22 19:14:40 +0630

    add update and delete function

--- controllers/posts.js
+++ controllers/posts.js
@@ -54,7 +54,7 @@ exports.renderCreatePage = (req,res,next)=>{
 
 // --get all post using sequlize--
 exports.getAllPosts = (req,res,next)=>{
-    PostModel.findAll()
+    PostModel.findAll({order : [["updatedAt","DESC"]]})
     .then((posts)=>{
         res.render("posts",{title:'Posts',posts})
     })
@@ -79,8 +79,49 @@ exports.getPost = (req,res,next)=>{
     //const post = posts.filter(post=>+post.id === +req.params.postID);
     PostModel.findOne({where : {id : +req.params.postID}})
       .then(post=>{
-          res.render("detail",{title:'Detail',post})
+          res.render("detail",{title:`${post.title}`,post})
       })
       .catch(err=>console.log(err))
     
   }
+
+exports.deletePost = (req,res) =>{
+    const postID = +req.params.postID;
+    PostModel.findByPk(postID)
+    .then((post)=>{
+        if(!post){
+            res.redirect("/posts")
+        }
+        return post.destroy();
+    })
+    .then(()=>{
+        console.log("Delete Successful!");
+        res.redirect("/posts")
+    })
+    .catch(err=>console.log(err))
+}
+
+exports.getOldPost = (req,res)=>{
+    const postID = +req.params.postID;
+    PostModel.findByPk(postID)
+    .then((post)=>{
+        res.render("editPost",{title:`${post.title}`,post})
+    })
+    .catch(err=>console.log(err))
+}
+
+exports.updatePost = (req,res)=>{
+    const {title,imgURL,description,postID} = req.body;
+    PostModel.findByPk(postID)
+    .then((post)=>{
+        post.title = title,
+        post.description = description,
+        post.imgUrl = imgURL
+        return post.save()
+    })
+    .then(result=>{
+        console.log(`Post ID ${postID} successfully updated.`)
+        res.redirect("/posts");
+    })
+    .catch(err=>console.log(err))
+}
--- routes/admin.js
+++ routes/admin.js
@@ -7,4 +7,10 @@ router.get("/create-post",postsController.renderCreatePage)
 
 router.post("/",postsController.createPost)
 
+router.post("/post/:postID",postsController.deletePost)
+
+router.get("/post-edit/:postID",postsController.getOldPost)
+
+router.post("/post-edit",postsController.updatePost)
+
 module.exports = {adminRoutes:router};
